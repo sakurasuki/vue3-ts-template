7b832850aa5610c0dc487948838cefc57458fe75
QiuSu <1048097942@qq.com>
2022-05-10 14:42:58 +0800

    修复toast函数错误问题&loading动画无法关闭问题

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,26 +1,26 @@
 // 自动按需导入vant不会导入这种vant函数 需要手动引入组件 + 样式
-import "vant/es/popup/index.less";
-import "vant/es/toast/index.less";
-import { Toast } from "vant";
-import type { ToastType, ToastOptions } from "vant";
+import 'vant/es/popup/index.less';
+import 'vant/es/toast/index.less';
+import { Toast } from 'vant';
+import type { ToastType, ToastOptions } from 'vant';
 export { Toast };
-export { get, post, request, formData } from "./request";
+export { get, post, request, formData } from './request';
 /**
  * toast文字提示
- * @param {object|string} msg
+ * @param {object|string} msgOptions
  */
-export const toast = (msgOptions: string | ToastOptions, type: ToastType = "text", duration = 0, forbidClick = true, icon = "") => {
-  if (msgOptions instanceof String) Toast({ message: msgOptions as string, type, duration, forbidClick, icon });
-  else Toast(msgOptions);
+export const toast = (msgOptions: string | ToastOptions, type: ToastType = 'text', duration = 1500, forbidClick = true, icon = '') => {
+  Toast.allowMultiple();
+  if (msgOptions instanceof Object) Toast({ type, duration, forbidClick, icon, ...(msgOptions as ToastOptions) });
+  else Toast({ message: msgOptions, type, duration, forbidClick, icon });
   return Toast;
 };
-
 // 获取本地静态图片
 export const getImageUrl = (fileName: string) => new URL(`../assets/imgs/${fileName}`, import.meta.url).href;
 
 // 格式化时间
-import moment from "moment";
-export const formatTime = (date: Date | string | number, format = "YYYY-MM-DD") => moment(date).format(format);
+import moment from 'moment';
+export const formatTime = (date: Date | string | number, format = 'YYYY-MM-DD') => moment(date).format(format);
 
 // 拼接图片地址
 const { VITE_APP_URL_IMG } = import.meta.env;
@@ -29,7 +29,7 @@ export const loadImg = (objectKey: string) => {
   try {
     return VITE_APP_URL_IMG + objectKey;
   } catch (error) {
-    return "https://www.baidu.com/error.jpg";
+    return 'https://www.baidu.com/error.jpg';
   }
 };
 /**
@@ -40,5 +40,5 @@ type Events = {
   handleClickMenu: number;
 };
 
-import mitt, { Emitter } from "mitt";
+import mitt, { Emitter } from 'mitt';
 export const emitter: Emitter<Events> = mitt<Events>();
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,21 +1,21 @@
 // 首先引入axios和上一步封装的cookie方法
-import axios from "axios";
-import type { AxiosRequestHeaders, AxiosResponse } from "axios";
+import axios from 'axios';
+import type { AxiosRequestHeaders, AxiosResponse } from 'axios';
 const { VITE_BASE_URL } = import.meta.env;
-import { toast, Toast } from "./index";
-import base from "@/base";
-import { Form } from "vant";
+import { toast, Toast } from './index';
+import base from '@/base';
+import { Form } from 'vant';
 const service = axios.create({
-  baseURL: VITE_BASE_URL,
+  baseURL: VITE_BASE_URL
   // timeout: 10000, // request timeout
 });
 // service.defaults.withCredentials = true;//表示跨域请求时是否需要使用凭证
 service.interceptors.request.use(
   (config) => {
     // (config.headers as any)[base.tokenKey] = sessionStorage.getItem(base.tokenKey) || "";
-    (config.headers as any).Authorization = "d2FwOlpNOUtuNU50SA==";
+    (config.headers as any).Authorization = 'd2FwOlpNOUtuNU50SA==';
     (config.headers as any).token =
-      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJpc3N1c2VyIiwiYXVkIjoiYXVkaWVuY2UiLCJ0ZW5hbnRfaWQiOiIwMDAwMDAiLCJyb2xlX25hbWUiOiJ1c2VyIiwidXNlcl9pZCI6IjYxODY0IiwidXNlcl9uYW1lIjoiMzIxMzIxMzEiLCJuaWNrX25hbWUiOiLmnY7plb_pnZIiLCJvYXV0aF9pZCI6IjE1MTQ0OTQxOTM1MjE3MDA4NjUiLCJkZXRhaWwiOnsiaWQiOjYxODY0LCJwaWQiOjAsInBwaWQiOjAsImFjY291bnQiOiIyODc2NDYwIiwibW9iaWxlIjoiMzIxMzIxMzEiLCJuaWNrTmFtZSI6IuadjumVv-mdkiIsInJlYWxOYW1lIjoi5p2O6ZW_6Z2SIiwiaWRDYXJkIjoiNDMwNTI0MTk2NDAyMjkwNzIxIiwiaWRDYXJkRnJvbnQiOiJodHRwczovL3d3dy5leG9zb21lZGVtb24uY29tL3VwbG9hZC9pbWFnZS8yMDIyMDQxNC82NDE4NjM5NjQzNjMwNy5qcGciLCJpZENhcmRCYWNrIjoiaHR0cHM6Ly93d3cuZXhvc29tZWRlbW9uLmNvbS91cGxvYWQvaW1hZ2UvMjAyMjA0MTQvNjQxODk5ODM4NDY3MzUuanBnIiwiaXNBdXRoIjoxLCJhdmF0YXIiOiJodHRwczovL3RoaXJkd3gucWxvZ28uY24vbW1vcGVuL3ZpXzMyLzZnUk5uYzZpYkRIeEE0RkozY29HTjZWbmJpYW5tMVNDc3A3RHpxd254N1VmeENsTEx0SzBPQ1NpY1M4SGVIaWFEalViQnpJUzdibnkzUjh5UkNFbVM0R21sdy8xMzIiLCJjaGFpbiI6IjAiLCJzdGF0dXMiOjEsImxldmVsIjowLCJtb25leSI6MC4wLCJtb25leVRvdGFsIjowLjAsImludGVncmFsIjowLjAsImludGVncmFsVG90YWwiOjAuMCwic2hhcmVDb2RlVXJsIjpudWxsLCJpc1ZhbGlkIjoxLCJjcmVhdGVUaW1lIjoiMjAyMi0wNC0xNCAxNDo0MjowOCIsInVwZGF0ZVRpbWUiOiIyMDIyLTA0LTE0IDE0OjQyOjA4IiwiaXNEZWxldGVkIjowLCJ2ZXJzaW9uIjowfSwidG9rZW5fdHlwZSI6ImFjY2Vzc190b2tlbiIsImFjY291bnQiOiIzMjEzMjEzMSIsImNsaWVudF9pZCI6IndhcCIsImV4cCI6MTY1MTYzMzIzNywibmJmIjoxNjUxMDI4NDM3fQ.oSRhwSoI1eNFt3wjCTj4IicozqJJhczyE8w8yLUBYyI";
+      'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJpc3N1c2VyIiwiYXVkIjoiYXVkaWVuY2UiLCJ0ZW5hbnRfaWQiOiIwMDAwMDAiLCJyb2xlX25hbWUiOiJ1c2VyIiwidXNlcl9pZCI6IjYxODY0IiwidXNlcl9uYW1lIjoiMzIxMzIxMzEiLCJuaWNrX25hbWUiOiLmnY7plb_pnZIiLCJvYXV0aF9pZCI6IjE1MTQ0OTQxOTM1MjE3MDA4NjUiLCJkZXRhaWwiOnsiaWQiOjYxODY0LCJwaWQiOjAsInBwaWQiOjAsImFjY291bnQiOiIyODc2NDYwIiwibW9iaWxlIjoiMzIxMzIxMzEiLCJuaWNrTmFtZSI6IuadjumVv-mdkiIsInJlYWxOYW1lIjoi5p2O6ZW_6Z2SIiwiaWRDYXJkIjoiNDMwNTI0MTk2NDAyMjkwNzIxIiwiaWRDYXJkRnJvbnQiOiJodHRwczovL3d3dy5leG9zb21lZGVtb24uY29tL3VwbG9hZC9pbWFnZS8yMDIyMDQxNC82NDE4NjM5NjQzNjMwNy5qcGciLCJpZENhcmRCYWNrIjoiaHR0cHM6Ly93d3cuZXhvc29tZWRlbW9uLmNvbS91cGxvYWQvaW1hZ2UvMjAyMjA0MTQvNjQxODk5ODM4NDY3MzUuanBnIiwiaXNBdXRoIjoxLCJhdmF0YXIiOiJodHRwczovL3RoaXJkd3gucWxvZ28uY24vbW1vcGVuL3ZpXzMyLzZnUk5uYzZpYkRIeEE0RkozY29HTjZWbmJpYW5tMVNDc3A3RHpxd254N1VmeENsTEx0SzBPQ1NpY1M4SGVIaWFEalViQnpJUzdibnkzUjh5UkNFbVM0R21sdy8xMzIiLCJjaGFpbiI6IjAiLCJzdGF0dXMiOjEsImxldmVsIjowLCJtb25leSI6MC4wLCJtb25leVRvdGFsIjowLjAsImludGVncmFsIjowLjAsImludGVncmFsVG90YWwiOjAuMCwic2hhcmVDb2RlVXJsIjpudWxsLCJpc1ZhbGlkIjoxLCJjcmVhdGVUaW1lIjoiMjAyMi0wNC0xNCAxNDo0MjowOCIsInVwZGF0ZVRpbWUiOiIyMDIyLTA0LTE0IDE0OjQyOjA4IiwiaXNEZWxldGVkIjowLCJ2ZXJzaW9uIjowfSwidG9rZW5fdHlwZSI6ImFjY2Vzc190b2tlbiIsImFjY291bnQiOiIzMjEzMjEzMSIsImNsaWVudF9pZCI6IndhcCIsImV4cCI6MTY1MTYzMzIzNywibmJmIjoxNjUxMDI4NDM3fQ.oSRhwSoI1eNFt3wjCTj4IicozqJJhczyE8w8yLUBYyI';
     return config;
   },
   (error) => Promise.reject(error)
@@ -24,7 +24,7 @@ service.interceptors.request.use(
 service.interceptors.response.use(
   (res) => {
     const status = res.data.code || res.status;
-    const message = res.data.massage || res.data.error_description || "未知错误";
+    const message = res.data.massage || res.data.error_description || '未知错误';
     if (status == 200) return Promise.resolve(res.data);
     // token过期
     else if (status == 1005) {
@@ -38,26 +38,23 @@ service.interceptors.response.use(
   }
 );
 
-// 加载动画
-const startLoading = () => {
-  toast({
-    type: "loading",
-    message: "加载中...",
-    loadingType: "spinner",
-    duration: 0,
-  });
+// loading实例列表
+let instanceofLoading: any[] = [];
+export const startLoading = () => {
+  instanceofLoading.push(Toast({ type: 'loading', message: '加载中...', loadingType: 'spinner', duration: 0 }));
 };
-const closeLoading = () => Toast.clear();
+// 关闭loading 遍历实例清除
+export const closeLoading = () => instanceofLoading.map((v) => v.clear());
 interface Response<T> {
   code: number;
   data: T;
   msg: string;
   success: boolean;
 }
-const request = <T>(type: "get" | "post", api: string, data: object, loading: boolean, headers: AxiosRequestHeaders = {}): Promise<Response<T>> => {
+const request = <T>(type: 'get' | 'post', api: string, data: object, loading: boolean, headers: AxiosRequestHeaders = {}): Promise<Response<T>> => {
   if (loading) startLoading();
   return new Promise((resolve, reject) => {
-    const query = type === "get" ? { params: { ...data } } : data;
+    const query = type === 'get' ? { params: { ...data } } : data;
     service[type](api, query, { headers })
       .then((res: AxiosResponse<Response<T>>) => resolve(Promise.resolve(res as any as Response<T>)))
       .catch((err) => reject(err))
@@ -65,11 +62,11 @@ const request = <T>(type: "get" | "post", api: string, data: object, loading: bo
   });
 };
 
-const get = <T = any>(api: string, params = {}, loading: boolean = false, headers = {}) => request<T>("get", api, params, loading, headers);
-const post = <T = any>(api: string, data = {}, loading: boolean = false, headers = {}) => request<T>("post", api, data, loading, headers);
+const get = <T = any>(api: string, params = {}, loading: boolean = false, headers = {}) => request<T>('get', api, params, loading, headers);
+const post = <T = any>(api: string, data = {}, loading: boolean = false, headers = {}) => request<T>('post', api, data, loading, headers);
 const formData = <T = any>(api: string, data: { [key: string]: any } = {}, loading: boolean = false) => {
   const fData = new FormData();
   Object.keys(data).forEach((k) => fData.append(k, data[k]));
-  return request<T>("post", api, fData, loading, { "Content-Type": "application/x-www-form-urlencoded" });
+  return request<T>('post', api, fData, loading, { 'Content-Type': 'application/x-www-form-urlencoded' });
 };
 export { get, post, request, formData };
